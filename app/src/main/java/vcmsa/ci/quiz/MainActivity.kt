fed62a01a703fc93437411a64b9be334bee895d2
ST10473173 <st10473173@vcconnect.edu.za>
2025-05-19 14:18:09 +0200

    intial

---
+++ app/src/main/java/vcmsa/ci/quiz/MainActivity.kt
@@ -0,0 +1,22 @@
+package vcmsa.ci.quiz
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnStart: Button = findViewById(R.id.btnStart) // Start button takes you to second screen
+        btnStart.setOnClickListener {
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
